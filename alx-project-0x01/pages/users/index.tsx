75857c83b0bb0b646f91c713ee78e9e9f7695f73
Michael Atolwa <atolwamichael7@gmail.com>
2025-10-02 12:45:21 +0300

    new commit

--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -7,4 +7,15 @@ const Users: React.FC = () => {
   )
 }
 
+export async function getStaticProps() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users")
+  const posts = await response.json()
+
+  return {
+    props: {
+      posts
+    }
+  }
+}
+
 export default Users;
